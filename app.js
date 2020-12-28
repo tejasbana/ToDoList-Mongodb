f8e78b33eba4166161495baff02ec676bfe1386f
tejasbana <tejasbana@gmail.com>
2020-12-28 19:07:59 +0530

    complete

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const date = require(__dirname + '/date.js');
+const _ = require("lodash");
 
 const app = express();
 app.use(express.static('public'));
@@ -63,7 +64,7 @@ app.get('/' , function(req , res){
 });
 
 app.get("/:customListName" , function(req , res){
-    const customListName = req.params.customListName;
+    const customListName = _.capitalize(req.params.customListName);
 
     List.findOne({name : customListName } , function(err , foundList){
         
@@ -112,13 +113,28 @@ app.post("/" , function(req , res){
 
 app.post("/delete" , function(req , res){
     const checkedItemId = req.body.checkbox;
-    Item.findByIdAndRemove(checkedItemId , function(err){
-        if(!err)
-        {
-            console.log("Successfuly deleted item");
-            res.redirect("/");
-        }
-    });
+    const listName = req.body.listName;
+
+    if(listName === "Today")
+    {
+        Item.findByIdAndRemove(checkedItemId , function(err){
+            if(!err)
+            {
+                console.log("Successfuly deleted item");
+                res.redirect("/");
+            }
+        });
+    }
+    else
+    {
+        List.findOneAndUpdate({name : listName} , {$pull : { items : {_id : checkedItemId}}} , function(err , foundList){
+            if(!err)
+            {
+                res.redirect('/'+listName);
+            }
+        });
+    }
+    
 });
 
 app.listen(3000 , function(){
