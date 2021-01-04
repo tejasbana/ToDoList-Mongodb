9c3195db9a81f7fc7b6afc41c4c5a2e2c19a937b
tejasbana <tejasbana@gmail.com>
2021-01-04 18:01:10 +0530

    Add Node Version

--- app.js
+++ app.js
@@ -139,6 +139,6 @@ app.post("/delete" , function(req , res){
     
 });
 
-app.listen(3000 , function(){
+app.listen(process.env.PORT || 3000 , function(){
     console.log("Server is up and running");
 });
